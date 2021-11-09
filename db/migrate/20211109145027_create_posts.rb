bf5dab8cc91f182e4e2f43b3139728dfb622a31a
Sergei Palmer <zigpalmer@gmail.com>
2021-11-09 14:56:49 +0000

    Posts scaffold

--- db/migrate/20211109145027_create_posts.rb
+++ db/migrate/20211109145027_create_posts.rb
@@ -1,7 +1,9 @@
-class CreatePosts < ActiveRecord::Migration[5.1]
+class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :message
+      t.integer :likes_count
+      t.string :image_url
 
       t.timestamps
     end
