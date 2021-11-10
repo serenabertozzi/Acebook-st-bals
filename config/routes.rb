b611ee9f0f1e79e7a6df3c0d1a124418746032e4
Sergei Palmer <zigpalmer@gmail.com>
2021-11-10 12:38:28 +0000

    User can register on website

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sign_up', to: "registrations#new"
+  post 'sign_up', to: "registrations#create"
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
