6dceab233532d0db127d4875da7b973d5d2e4d06
AJOsmaston <dajoxx@gmail.com>
2021-11-10 11:19:52 +0000

    added comment scaffold + assosiation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_145027) do
+ActiveRecord::Schema.define(version: 2021_11_10_110252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment_text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
