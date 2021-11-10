03d2f9296475feb99848b5ad225bbf59a49bdbaa
Tim Turvey <tturvey@hotmail.co.uk>
2021-11-10 15:39:16 +0000

    redirect to rootpath

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,15 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    
     if @user.save
       session[:user_id] = @user.id
+      p session[:user_id]
       redirect_to root_path, notice: "You successfully created an Acebook account"
     else
       render :new
     end
+    
   end
 
   private
