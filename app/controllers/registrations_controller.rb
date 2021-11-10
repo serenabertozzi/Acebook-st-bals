40f6e41190c7862a1a932fa49fc9b89c894c91b6
Sergei Palmer <zigpalmer@gmail.com>
2021-11-10 12:04:33 +0000

    create user controller method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,21 @@
 class RegistrationsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You successfully created an Acebook account"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    # https://api.rubyonrails.org/v6.1.4/classes/ActionController/Parameters.html#method-i-permit
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'registrations/new' , to: "registrations#new"
+  get 'sign_up', to: "registrations#new"
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
