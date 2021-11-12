81290409148a1957f6420200585788f2de555cd4
Sergei Palmer <zigpalmer@gmail.com>
2021-11-12 10:49:50 +0000

    Found the missing tag

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Sign Up", type: :feature do
   scenario "User can create an account" do
-    visit "/sign-up"
+    visit "/"
+    click_button "Sign Up"
     fill_in "user[first_name]", with: "Bilbo"
     fill_in "user[last_name]", with: "Baggins"
     fill_in "user[email]", with: "bilbo@theshire.com"
