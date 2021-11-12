a1d49760adbe43834db6a159e2bac9f2e5a6ceed
Bernard Leanse <bernardleanse96@gmail.com>
2021-11-12 14:32:14 +0000

    comments now visible on main page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-
+  before_action :get_post
   # GET /comments or /comments.json
   def index
     @comments = Comment.all
@@ -13,8 +13,10 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     # you can call @post.post_id to get the post this comment belongs to
-    @comment = Comment.new
-    # @post = Post.where("id = #{params[:post_id]}").first
+    @post
+    # @comment = @post.comments.new
+  
+    
   end
 
   # GET /comments/1/edit
@@ -23,8 +25,8 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
-    @post.comments.create(user_id: session[:user_id])
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.new(comment_params)
+    # @comment = Comment.new(comment_params)
 
     respond_to do |format|
       if @comment.save
@@ -69,5 +71,9 @@ class CommentsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def comment_params
     params.require(:comment).permit(:comment_text, :post_id)
+  end
+
+  def get_post
+    @post = Post.find(params[:post_id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @post = Post.new
+    
   end
 
   # GET /posts/1 or /posts/1.json
