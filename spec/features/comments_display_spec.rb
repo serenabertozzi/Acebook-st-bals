3ace54ac03beafee251290698e19cad2c85b16e7
Bernard Leanse <bernardleanse96@gmail.com>
2021-11-15 16:38:16 +0000

    comments time ago

--- spec/features/comments_display_spec.rb
+++ spec/features/comments_display_spec.rb
@@ -19,4 +19,13 @@ feature 'comments' do
     comment = find_by_id('commentCard')
     expect(comment).to have_css("img[src*='teamlogo.png']")
   end
+
+  scenario 'comments show time posted' do
+    register_and_sign_in
+    create_test_post
+    leave_test_comment
+
+    comment = find_by_id('commentCard')
+    expect(comment).to have_content("Posted less than a minute ago.")
+  end
 end
