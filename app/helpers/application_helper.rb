be573e8121bbc07f98a8b3bcb879d7d2900f4635
Tim Turvey <tturvey@hotmail.co.uk>
2021-11-18 17:02:50 +0000

    Able to post embedded youtube videos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def video_embed(video_url)
+
+    # REGEX EXTRACT VIDEO ID
+    regex_id = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/|vimeo\.com\/)([a-zA-Z0-9_-]{8,11})/
+    match_id = regex_id.match(video_url)
+    video_id = ""
+    if match_id && !match_id[1].blank?
+      video_id = match_id[1]
+    end
+
+    # REGEX EXTRACT PROVIDER - YOUTUBE OR VIMEO
+    regex_prov = /(youtube|youtu\.be|vimeo)/
+    match_prov = regex_prov.match(video_url)
+    video_prov = ""
+    if match_prov && !match_prov[1].blank?
+      video_prov = case match_prov[1]
+                     when "youtube"
+                       :youtube
+                     when "youtu.be"
+                       :youtube
+                     when "vimeo"
+                       :vimeo
+      end
+    end
+
+    case video_prov
+      when :youtube
+        "https://www.youtube.com/embed/#{video_id}"
+      when :vimeo
+        "https://player.vimeo.com/video/#{video_id}"
+    end
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_144616) do
+ActiveRecord::Schema.define(version: 2021_11_18_121859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_144616) do
     t.string "message"
     t.integer "likes_count"
     t.string "image_url"
+    t.string "video_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
