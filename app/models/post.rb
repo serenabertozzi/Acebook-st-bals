6a7a6bd8151e5e16858b900b93b62ecab07db9a5
Sergei Palmer <zigpalmer@gmail.com>
2021-11-18 11:18:30 +0000

    Extracted comments into posts.json response

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, -> { order(created_at: :desc)}, inverse_of: :post, dependent: :destroy
+  scope :with_comments, ->(include) { includes(:comments) if include.present? && include.to_bool }
   has_many :likes, dependent: :destroy
   has_many_attached :pictures, service: :local
   validates :message, presence: true
--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,9 @@
-json.extract! post, :id, :message, :likes_count, :image_url, :created_at, :updated_at
+json.extract! post, :id, :message, :likes_count, :created_at, :updated_at, :user_id
+
+if params.fetch(:comments, false).to_bool
+  json.comments post.comments do |comment|
+    json.partial! "comments/comment", comment: comment
+  end
+end
+
 json.url post_url(post, format: :json)
