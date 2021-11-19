86d3c136f95e722e8ac93a80f6a60a489a8c12dc
hamstercat007 <lisamac421@gmail.com>
2021-11-19 11:58:01 +0000

    Added editing functionality on comments modal form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to post_path(@post), notice: 'Comment was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit, status: :unprocessable_entity }
